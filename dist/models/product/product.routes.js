3d3b392e67c3bb23656ba4cd7d11fa657ea4c564
yeaseen22 <devilarmaan63@gmail.com>
2026-06-16 15:50:16 +0600

    feat: dropshipping search feature

--- dist/models/product/product.routes.js
+++ dist/models/product/product.routes.js
@@ -14,10 +14,12 @@ productRouter.post('/get', product_controllers_1.getProductController);
 productRouter.post("/get-product-by-category", product_controllers_1.getProductByCategory);
 productRouter.post('/get-pruduct-by-category-and-subcategory', product_controllers_1.getProductByCategoryAndSubCategory);
 productRouter.post('/get-product-details/:productId', product_controllers_1.getProductDetails);
-//update product
+// update product
 productRouter.put('/update-product-details', isAuth_1.isAuth, isAdmin_1.isAdmin, product_controllers_1.updateProductDetails);
-//delete product
+// delete product
 productRouter.delete('/delete-product', isAuth_1.isAuth, isAdmin_1.isAdmin, product_controllers_1.deleteProductDetails);
-//search product 
+// text / sky-title search
 productRouter.post('/search-product', product_controllers_1.searchProduct);
+// image search (uses sharp locally – no Cloudinary)
+productRouter.post('/image-search', multer_1.upload.single('searchImage'), product_controllers_1.imageSearchProduct);
 exports.default = productRouter;
